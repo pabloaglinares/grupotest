3289784c6d3f343d3c2cd5a1f8f2a1d54fa3660f
USUARIO <USUARIO@10.0.31.254>
2014-01-16 12:30:47 +0100

    modificada clase Test

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -14,6 +14,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hola");
         // TODO code application logic here
     }
 }
